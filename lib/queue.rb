df086ff898eeddda7dfb10d6c186dcbb4bd5b83b
laurpaik <laurpaik@gmail.com>
2017-03-15 11:39:48 -0400

    Annotate Antony's code

--- lib/queue.rb
+++ lib/queue.rb
@@ -2,4 +2,25 @@
 
 # A queue class
 class Fifo
+  # allocates space for items on our queue
+  def initialize
+    @store = []
+  end
+
+  # Is this queue empty?
+  def empty?
+    @store.empty?
+  end
+
+  # Adds items to the end of the queue
+  # Returns self to allow method chaining
+  def enqueue(value)
+    @store.push(value)
+    self
+  end
+
+  # Removes item from the front of the queue
+  def dequeue
+    @store.shift
+  end
 end
